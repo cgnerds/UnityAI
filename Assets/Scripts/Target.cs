790c768278fa10b98a91166842003d4c02b5ecba
cgnerds <cgnerds@gmail.com>
2018-08-01 12:34:23 +0800

    Add Target.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+    public Transform targetMarker;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int button = 0;
+        // Get the point of the hit position when the mouse is being clicked 
+        if(Input.GetMouseButtonDown(button))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                targetMarker.position = targetPosition;
+            }
+        }
+	}
+}
