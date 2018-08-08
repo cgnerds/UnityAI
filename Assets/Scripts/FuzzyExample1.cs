b5886f4657d5747dc46f93eb524e8021707368fd
cgnerds <cgnerds@gmail.com>
2018-08-08 10:56:36 +0800

    Using fuzzy logic.

---
+++ Assets/Scripts/FuzzyExample1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuzzyExample1 : MonoBehaviour {
+    private const string labelText = "{0} true";
+    public AnimationCurve critical;
+    public AnimationCurve hurt;
+    public AnimationCurve healthy;
+
+    public InputField healthInput;
+    public Text healthyLabel;
+    public Text hurtLabel;
+    public Text criticalLabel;
+
+    private float criticalValue = 0f;
+    private float hurtValue = 0f;
+    private float healthyValue = 0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+        SetLabels();
+	}
+
+    // Evaluates all the curves and returns float values
+    public void EvaluateStatements()
+    {
+        if(string.IsNullOrEmpty(healthInput.text))
+        {
+            return;
+        }
+
+        float inputValue = float.Parse(healthInput.text);
+        healthyValue = healthy.Evaluate(inputValue);
+        hurtValue = hurt.Evaluate(inputValue);
+        criticalValue = critical.Evaluate(inputValue);
+
+        SetLabels();
+    }
+	
+    // Updates the GUI with the evaluated values based on the health percentage by the user
+    private void SetLabels()
+    {
+        healthyLabel.text = string.Format(labelText, healthyValue);
+        hurtLabel.text = string.Format(labelText, hurtValue);
+        criticalLabel.text = string.Format(labelText, criticalValue);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
